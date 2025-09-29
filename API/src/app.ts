0389843360006fa1070e009e046662640a21febb
robnez44 <00044623@uca.edu.sv>
2025-09-29 01:13:04 -0600

    cors

--- API/src/app.ts
+++ API/src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import commentRoutes from './routes/comment.router'
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET','POST','OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+}));
+
 
 app.use(express.json());
 app.use(commentRoutes);
